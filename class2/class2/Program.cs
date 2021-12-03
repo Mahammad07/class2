9d8f882bfe644a46a0f5b13e5174ea3e6b8ffde5
Mahammad07 <94453965+Mahammad07@users.noreply.github.com>
2021-12-03 00:35:12 -0600

    first

---
+++ class2/class2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Abdullah";
+            employee.Surname = "Dasdemirov";
+            employee.Position = "Bagban";
+            employee.DailySalary = 10;
+            employee.MonthSalary(employee.Name, employee.Surname, employee.Position, employee.DailySalary);
+        }
+    }
+
+
+    class Employee
+    {
+        public string Name;
+            public string Surname;
+        public string Position;
+        public int DailySalary;
+
+        public void MonthSalary(string name,string surname,string position,int dailySalary)
+        {
+            Console.WriteLine($"{name} {surname} {position} {dailySalary*30}");
+        }
+    }
+
+       
+
+}
